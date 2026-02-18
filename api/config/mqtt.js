90c5d7789f770127c86b7575542f9a807274126c
ZQDR <iszqdr@gmail.com>
2026-02-18 12:59:36 +0100

    Test api arch

--- api/config/mqtt.js
+++ api/config/mqtt.js
@@ -16,7 +16,7 @@ console.log(`1. ENVIRONNEMENT DÉTECTÉ : ${process.env.DB_HOST === 'db' ? 'DOCK
 console.log("2. VALEURS REÇUES :");
 console.log(`   - HOST: '${process.env.DB_HOST}'`);
 console.log(`   - USER: '${process.env.DB_USER}'`);
-console.log(`   - PASS: '${process.env.DB_PASS ? '****** (Masqué)' : '❌ ABSENT'}'`);
+console.log(`   - PASS: '${process.env.DB_PASS ? '****** (Masquer)' : '❌ ABSENT'}'`);
 console.log(`   - NAME: '${process.env.DB_NAME}'`);
 console.log(`   - PORT: '${process.env.DB_PORT}'`);
 
