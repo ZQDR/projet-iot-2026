b4a5bf4a7fa1f37568e20c08bb23d4a9b083c2b2
ZQDR <iszqdr@gmail.com>
2026-03-17 09:59:06 +0100

    Test

--- api/services/socketService.js
+++ api/services/socketService.js
@@ -7,6 +7,7 @@ module.exports = {
     // 1. Initialisation (Appelée une seule fois par index.js au démarrage)
     init: (httpServer) => {
         io = new Server(httpServer, {
+            path: "/api/socket.io",
             cors: {
                 origin: "*", // On autorise tout le monde pour éviter les blocages
                 methods: ["GET", "POST"]
